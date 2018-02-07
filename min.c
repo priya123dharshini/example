fb23a3d01bac8e5a4a472f00df079696336883f8
priya123dharshini <30829459+priya123dharshini@users.noreply.github.com>
2018-02-07 16:23:28 +0530

    Create README.md

---
+++ min.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+ int i,n,smallest;
+ printf("enter numbers");
+ for(i=0;i<n;i++)
+ {
+ scanf("%d",&a[i]);
+ }
+ a[0]=smallest;
+ for(i=0;i<n;i++)
+ {
+  if(a[i]<smallest)
+   {
+    smallest=a[i];
+    }
+  }
+  printf("%d",smallest);
+  }
+  
+  
